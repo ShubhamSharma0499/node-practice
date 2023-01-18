5a560ef0419db8cfde20f39b9e47a3be4baa31e3
ShubhamSharma0499 <shubham.eng999@gmail.com>
2023-01-18 18:21:25 +0530

    setting up basic express router

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const express=require('express');
 
 const app=express();
 const bodyParser=require('body-parser') // user for parsing req
-
+const homeRoutes=require('./routes/home'); //importing home routes
 
 app.use(bodyParser.urlencoded({extended:false})); // this middleware allows us to parse data from request now we can do req.body and get data passed in request body
+ 
+app.use(homeRoutes)
 
-// add middleware
-app.use((req,res,next)=>{ // this function will be called for every request 
-    console.log('middleware called');
-    next(); //this is used to pass control to next middleware else there will no furthur process take place
-})
-// note either you call next() or you should return a response in middleware to pass control 
 
-app.get('/',(req,res)=>{
-    return res.send('data')
-})
+
+
+
+
+
 
 
 app.listen(3000,(err)=>{
