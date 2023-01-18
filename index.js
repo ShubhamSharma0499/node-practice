1326ef2361adbab5a15578efe55df95c0968da01
ShubhamSharma0499 <shubham.eng999@gmail.com>
2023-01-18 17:52:32 +0530

    learned about middlewares

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express=require('express');
 
 const app=express();
 
+
+
+// add middleware
+app.use((req,res,next)=>{ // this function will be called for every request 
+    console.log('middleware called');
+    next(); //this is used to pass control to next middleware else there will no furthur process take place
+})
+
+
 app.get('/',(req,res)=>{
     return res.send('data')
 })
+
 
 app.listen(3000,(err)=>{
     if(err){
