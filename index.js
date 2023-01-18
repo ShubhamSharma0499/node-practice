5e14e2e0ee6f784479224acfff7da3892d99cedc
ShubhamSharma0499 <shubham.eng999@gmail.com>
2023-01-18 18:27:39 +0530

    adding path for serving routes file based on that path

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const homeRoutes=require('./routes/home'); //importing home routes
 
 app.use(bodyParser.urlencoded({extended:false})); // this middleware allows us to parse data from request now we can do req.body and get data passed in request body
  
-app.use(homeRoutes)
+app.use('/',homeRoutes)
 
 
 
