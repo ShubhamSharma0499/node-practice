6662c11f4efc88198be58eda6e7a20f82b704130
ShubhamSharma0499 <shubham.eng999@gmail.com>
2023-01-18 17:54:14 +0530

    learned about middlewares adding notes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use((req,res,next)=>{ // this function will be called for every request
     console.log('middleware called');
     next(); //this is used to pass control to next middleware else there will no furthur process take place
 })
-
+// note either you call next() or you should return a response in middleware to pass control 
 
 app.get('/',(req,res)=>{
     return res.send('data')
