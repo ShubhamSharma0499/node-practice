88755a60d7f4444cd030a402f09f7ba45f3bc0b3
ShubhamSharma0499 <shubham.eng999@gmail.com>
2023-01-18 17:46:39 +0530

    created basic express server

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const http=require("http");
+const express=require('express');
 
+const app=express();
 
-const server=http.createServer((req,res)=>{
-console.log(req);
+app.get('/',(req,res)=>{
+    return res.send('data')
 })
 
-server.listen(3000);
+app.listen(3000,(err)=>{
+    if(err){
+        console.log('error while running server ')
+        return;
+    }
+    console.log('server is running');
+})
