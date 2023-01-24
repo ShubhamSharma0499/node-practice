0081f81d04f3b5b750b2f8dd1cec7657af08c96c
ShubhamSharma0499 <shubham.eng999@gmail.com>
2023-01-24 09:47:46 +0530

    connected node to mysql andlearned how to retreive data from table

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express=require('express');
 const app=express();
 const bodyParser=require('body-parser') // user for parsing req
 const homeRoutes=require('./routes/user'); //importing home routes
+const db=require('./config/sql');
+
+db.execute('SELECT * from users').then(res=>console.log(res)).catch(err=>console.log(err))
+
 
 app.use(bodyParser.urlencoded({extended:false})); // this middleware allows us to parse data from request now we can do req.body and get data passed in request body
  
