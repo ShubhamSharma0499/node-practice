fef621a86d6fac11210482765821c7fbfc9587fa
ShubhamSharma0499 <shubham.eng999@gmail.com>
2023-01-24 12:47:57 +0530

    created tables for our models in sequelize with .sync() function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express=require('express');
 const app=express();
 const bodyParser=require('body-parser') // user for parsing req
 const homeRoutes=require('./routes/user'); //importing home routes
-const db=require('./config/sql');
+const sequelize=require('./config/sql');
 
 
 app.use(bodyParser.urlencoded({extended:false})); // this middleware allows us to parse data from request now we can do req.body and get data passed in request body
@@ -11,9 +11,11 @@ app.use(bodyParser.json());
  
 app.use('/user',homeRoutes)
 
+sequelize.sync().then(res=>{}).catch(err=>{}) // it creates tables for models we created using sequelize
+
 app.listen(3000,(err)=>{
     if(err){
-        console.log('error while running server ')
+        console.log('error while running server ') 
         return;
     }
     console.log('server is running');
